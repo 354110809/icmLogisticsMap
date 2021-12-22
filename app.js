09b217c968af2a42b823c92200d538c61f3b2e6f
班未东 <354110809@qq.com>
2021-12-22 11:11:29 +0800

    feat: 修改node启动端口

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ app.use(express.static(path.resolve(__dirname, './dist')))
 
 
 //监听3002端口
-app.listen(3000, '0.0.0.0', () => {
-    console.log('server is running 3000');
+app.listen(5555, '0.0.0.0', () => {
+    console.log('项目启动成功：端口5555');
 });
